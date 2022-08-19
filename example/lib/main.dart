0cd1161f917a0c771195a6e364d9485e87d16650
ak78887 <ankurmaurya286@gmail.com>
2022-08-19 14:05:12 +0530

    initial

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:crypto_market/Crypto_Market/Model/coin_model.dart';
+import 'package:crypto_market/crypto_market.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,103 +15,173 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Crypto Market'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Container(
+        padding: const EdgeInsets.only(top: 20),
+        child: allCoin(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget allCoin() {
+    return AllCoin(
+      coinsList: coinsList,
+      currencyList: currencyList,
+      tickerList: tickerList,
+      showWishlistAtFirst: false,
+      currencyTabSelectedItemColor: Colors.red,
+      currencyTabBackgroundColor: Colors.transparent,
+      currencyTabHeight: 100,
+      showHeading: true,
+      inrRate: 77.0,
+      onCoinTap: (ctx, coin) {
+        ///  ------  ///
+      },
+    );
+  }
+
+  Widget coinGraph() {
+    return CoinGraph(
+      coinData: coinsList.elementAt(0),
+      listedCoinGraphUrl: 'http://node.demo.com/orders/getohlc?symbol=TSTUSDT&interval=1m',
+      inrRate: 77.0,
+    );
+  }
+
+  Widget orderBook() {
+    return  CoinOrderBook(
+      coinData: coinsList.elementAt(0),
+      listedCoinOrderBookUrl: 'http://node.demo.com/orders/order-book?currency=TST&with_currency=USDT',
+      inrRate: 77.0,
+    );
+  }
+
+  Widget orderVolume() {
+    return CoinOrderVolume(
+      coinData: coinsList.elementAt(0),
+      listedCoinOrderBookUrl: 'http://node.demo.com/orders/order-book?currency=TST&with_currency=USDT',
+      inrRate: 77.0,
+    );
+  }
+
+  Widget tradeHistory() {
+    return CoinTradeHistory(
+      coinData: coinsList.elementAt(0),
+      listedCoinTradeHistoryAPIUrl: 'http://node.demo.com/orders/trade-book?currency=TST&with_currency=USDT',
+      itemCount: 15,
+      inrRate: 77,
+    );
+  }
+
+  Widget coinSearch() {
+    return CoinSearch(
+      coinsList: coinsList,
+      currencyList: currencyList,
+      tickerList: tickerList,
+      inrRate: 77.0,
+    );
+  }
+
 }
+
+
+
+
+List<Coin> coinsList = [
+  Coin(coinID: '1', coinImage: 'https://', coinName: 'Bitcoin', coinShortName: 'BTC', coinPrice: '123456', coinLastPrice: '123456', coinPercentage: '-0.5', coinSymbol: 'BTCUSDT', coinPairWith: 'USDT', coinHighDay: '567', coinLowDay: '12', coinDecimalPair: '3', coinDecimalCurrency: '4', coinListed: false),
+  Coin(coinID: '2', coinImage: 'https://', coinName: 'Bitcoin', coinShortName: 'BTC', coinPrice: '123456', coinLastPrice: '123456', coinPercentage: '-0.5', coinSymbol: 'BTCINR', coinPairWith: 'INR', coinHighDay: '567', coinLowDay: '12', coinDecimalPair: '3', coinDecimalCurrency: '4', coinListed: false),
+  Coin(coinID: '3', coinImage: 'https://', coinName: 'Binance USD', coinShortName: 'BUSD', coinPrice: '0.0005', coinLastPrice: '0.0005', coinPercentage: '-0.5', coinSymbol: 'BUSDBNB', coinPairWith: 'BNB', coinHighDay: '567', coinLowDay: '12', coinDecimalPair: '3', coinDecimalCurrency: '4', coinListed: false),
+  Coin(coinID: '4', coinImage: 'https://', coinName: 'Dogecoin', coinShortName: 'DOGE', coinPrice: '123456', coinLastPrice: '123456', coinPercentage: '-0.5', coinSymbol: 'DOGEUSDT', coinPairWith: 'USDT', coinHighDay: '567', coinLowDay: '12', coinDecimalPair: '3', coinDecimalCurrency: '4', coinListed: false),
+];
+
+List<String> currencyList = [
+  'USDT',
+  'INR',
+  'BNB',
+];
+
+
+
+List<String> tickerList = [
+  "btcusdt@ticker",
+  "ethusdt@ticker",
+  "winusdt@ticker",
+  "dentusdt@ticker",
+  "xrpusdt@ticker",
+  "etcusdt@ticker",
+  "dogeusdt@ticker",
+  "bnbusdt@ticker",
+  "yfiusdt@ticker",
+  "cakeusdt@ticker",
+  "vetusdt@ticker",
+  "maticusdt@ticker",
+  "trxusdt@ticker",
+  "eosusdt@ticker",
+  "usdcusdt@ticker",
+  "neoeth@ticker",
+  "xmrbtc@ticker",
+  "wintrx@ticker",
+  "yfiiusdt@ticker",
+  "aaveusdt@ticker",
+  "dotusdt@ticker",
+  "sandusdt@ticker",
+  "maticbtc@ticker",
+  "polybtc@ticker",
+  "yfiibtc@ticker",
+  "bnbbtc@ticker",
+  "yfibtc@ticker",
+  "aavebtc@ticker",
+  "ltcbtc@ticker",
+  "cakebtc@ticker",
+  "eosbtc@ticker",
+  "jstbtc@ticker",
+  "chzbtc@ticker",
+  "polybtc@ticker",
+  "solbtc@ticker",
+  "ksmbtc@ticker",
+  "compbtc@ticker",
+  "dashbtc@ticker",
+  "axsbtc@ticker",
+  "btgbtc@ticker",
+  "lunabtc@ticker",
+  "dasheth@ticker",
+  "avaxeth@ticker",
+  "axseth@ticker",
+  "etceth@ticker",
+  "doteth@ticker",
+  "linketh@ticker",
+  "omgeth@ticker",
+  "sandeth@ticker",
+  "waveseth@ticker",
+  "nanoeth@ticker",
+  "ezeth@ticker",
+  "manaeth@ticker",
+  "enjeth@ticker",
+  "lsketh@ticker",
+  "aaveeth@ticker",
+  "mtleth@ticker",
+  "adaeth@ticker",
+  "iotaeth@ticker",
+  "xrpeth@ticker",
+  "shibusdt@ticker",
+];
